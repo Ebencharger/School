31924e691ffd643f929543c83036ad4f87995616
Ebencharger <ajayioluwaseunebenezer@gmail.com>
2022-02-17 16:21:36 +0000

    media for login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,9 +13,10 @@
 
     <!-- Styles -->
     <style>
+
         .login {
                width: 100%;
-               /* height: 100vh; */
+               height: 100vh;
                display: flex;
            }
            .login form{
@@ -62,6 +63,407 @@
             border: none;
             border-radius: 0px;
             border-bottom: 2px solid #007bff;
+        }
+        @media(max-width:915px){
+            .login{
+                width: 100%;
+                padding: 0px !important;
+                overflow-y: scroll;
+                margin-bottom: 10px;
+
+            }
+            .login .image img{
+              display: none;
+           }
+           .login form{
+               padding-left: 0px !important;
+               padding: 0px !important;
+               width: 100% !important;
+               margin-left:10%;
+               margin-top: 5%;
+           }
+           .login form h2{
+               position: absolute;
+               top: 10%;
+               left: 38%;
+           }
+           #login input {
+            outline: none;
+            width: 600px;
+            width: 100% !important;
+            padding: 4px;
+            margin-left: 0px;
+            border: none;
+            border-radius: 0px;
+            border-bottom: 2px solid #007bff;
+        }
+        }
+        @media(max-width:915px){
+            .login{
+                width: 100%;
+                padding: 0px !important;
+                overflow-y: scroll;
+                margin-bottom: 10px;
+
+            }
+            .login .image img{
+              display: none;
+           }
+           .login form{
+               padding-left: 0px !important;
+               padding: 0px !important;
+               width: 100% !important;
+               margin-left:10%;
+               margin-top: 5%;
+           }
+           .login form h2{
+               position: absolute;
+               top: 10%;
+               left: 38%;
+           }
+           #login input {
+            outline: none;
+            width: 600px;
+            width: 100% !important;
+            padding: 4px;
+            margin-left: 0px;
+            border: none;
+            border-radius: 0px;
+            border-bottom: 2px solid #007bff;
+        }
+        }
+        @media(max-width:914px){
+            .login{
+                width: 100%;
+                padding: 0px !important;
+                overflow-y: scroll;
+                margin-bottom: 10px;
+
+            }
+            .login .image img{
+              display: none;
+           }
+           .login form{
+               padding-left: 0px !important;
+               padding: 0px !important;
+               width: 100% !important;
+               margin-left:10%;
+               margin-top: 10%;
+           }
+           .login form h2{
+               position: absolute;
+               top: 20%;
+               left: 38%;
+           }
+           #login input {
+            outline: none;
+            width: 600px;
+            width: 100% !important;
+            padding: 4px;
+            margin-left: 0px;
+            border: none;
+            border-radius: 0px;
+            border-bottom: 2px solid #007bff;
+        }
+        }
+        @media(max-width:912px){
+            .login{
+                width: 100%;
+                padding: 0px !important;
+                overflow-y: scroll;
+                margin-bottom: 10px;
+
+            }
+            .login .image img{
+              display: none;
+           }
+           .login form{
+               padding-left: 0px !important;
+               padding: 0px !important;
+               width: 100% !important;
+               margin-left:10%;
+               margin-top: 40% !important;
+           }
+           .login form h2{
+               position: absolute;
+               top: 20%;
+               left: 38%;
+           }
+           #login input {
+            outline: none;
+            width: 600px;
+            width: 100% !important;
+            padding: 4px;
+            margin-left: 0px;
+            border: none;
+            border-radius: 0px;
+            border-bottom: 2px solid #007bff;
+        }
+        }
+        @media(max-width:896px){
+            .login{
+                width: 100%;
+                padding: 0px !important;
+                margin-bottom: 10px;
+            }
+            .login .image img{
+              display: none;
+           }
+           .login form{
+               padding-left: 0px !important;
+               padding: 0px !important;
+               width: 100% !important;
+               margin-left:10%;
+               margin-top: 10% !important;
+           }
+           .login form h2{
+               position: absolute;
+               top: 10%;
+               left: 38%;
+           }
+           #login input {
+            outline: none;
+            width: 600px;
+            width: 100% !important;
+            padding: 4px;
+            margin-left: 0px;
+            border: none;
+            border-radius: 0px;
+            border-bottom: 2px solid #007bff;
+        }
+        }
+        @media(max-width:820px){
+            .login{
+                width: 100%;
+                padding: 0px !important;
+                overflow-y: scroll;
+                margin-bottom: 10px;
+
+            }
+            .login .image img{
+              display: none;
+           }
+           .login form{
+               padding-left: 0px !important;
+               padding: 0px !important;
+               width: 100% !important;
+               margin-left:10%;
+               margin-top: 45% !important;
+           }
+           .login form h2{
+               position: absolute;
+               top: 20%;
+               left: 38%;
+           }
+           #login input {
+            outline: none;
+            width: 600px;
+            width: 100% !important;
+            padding: 4px;
+            margin-left: 0px;
+            border: none;
+            border-radius: 0px;
+            border-bottom: 2px solid #007bff;
+        }
+        }
+        @media(max-width:740px){
+            .login{
+                width: 100%;
+                padding: 0px !important;
+                overflow-y: scroll;
+                margin-bottom: 10px;
+
+            }
+            .login .image img{
+              display: none;
+           }
+           .login form{
+               padding-left: 0px !important;
+               padding: 0px !important;
+               width: 100% !important;
+               margin-left:10%;
+               margin-top: 10% !important;
+           }
+           .login form h2{
+               position: absolute;
+               top: 20%;
+               left: 38%;
+           }
+           #login input {
+            outline: none;
+            width: 600px;
+            width: 100% !important;
+            padding: 4px;
+            margin-left: 0px;
+            border: none;
+            border-radius: 0px;
+            border-bottom: 2px solid #007bff;
+        }
+        }
+        @media(max-width:720px){
+            .login{
+                width: 100%;
+                padding: 0px !important;
+                overflow-y: scroll;
+                margin-bottom: 10px;
+
+            }
+            .login .image img{
+              display: none;
+           }
+           .login form{
+               padding-left: 0px !important;
+               padding: 0px !important;
+               width: 100% !important;
+               margin-left:10%;
+               margin-top: 25% !important;
+           }
+           .login form h2{
+               position: absolute;
+               top: 20%;
+               left: 38%;
+           }
+           #login input {
+            outline: none;
+            width: 600px;
+            width: 100% !important;
+            padding: 4px;
+            margin-left: 0px;
+            border: none;
+            border-radius: 0px;
+            border-bottom: 2px solid #007bff;
+        }
+        }
+        @media(max-width:700px){
+            .login{
+                width: 100%;
+                padding: 0px !important;
+                overflow-y: scroll;
+                margin-bottom: 10px;
+
+            }
+            .login .image img{
+              display: none;
+           }
+           .login form{
+               padding-left: 0px !important;
+               padding: 0px !important;
+               width: 100% !important;
+               margin-left:10%;
+               margin-top: 15% !important;
+           }
+           .login form h2{
+               position: absolute;
+               top: 20%;
+               left: 38%;
+           }
+           #login input {
+            outline: none;
+            width: 600px;
+            width: 100% !important;
+            padding: 4px;
+            margin-left: 0px;
+            border: none;
+            border-radius: 0px;
+            border-bottom: 2px solid #007bff;
+        }
+        }
+        @media(max-width:653px){
+            .login{
+                width: 100%;
+                padding: 0px !important;
+                overflow-y: scroll;
+                margin-bottom: 10px;
+
+            }
+            .login .image img{
+              display: none;
+           }
+           .login form{
+               padding-left: 0px !important;
+               padding: 0px !important;
+               width: 100% !important;
+               margin-left:10%;
+               margin-top: 15%;
+           }
+           .login form h2{
+               position: absolute;
+               top: 20%;
+               left: 38%;
+           }
+           #login input {
+            outline: none;
+            width: 600px;
+            width: 100% !important;
+            padding: 4px;
+            margin-left: 0px;
+            border: none;
+            border-radius: 0px;
+            border-bottom: 2px solid #007bff;
+        }
+        }
+        @media(max-width:414px){
+            .login{
+                width: 100%;
+                padding: 0px !important;
+            }
+            .login .image img{
+              display: none;
+           }
+           .login form{
+               padding-left: 0px !important;
+               padding: 0px !important;
+               width: 100% !important;
+               margin-left:10%;
+               margin-top: 50% !important;
+           }
+           .login form h2{
+               position: absolute;
+               top: 20%;
+               left: 28%;
+           }
+           #login input {
+            outline: none;
+            width: 600px;
+            width: 100% !important;
+            padding: 4px;
+            margin-left: 0px;
+            border: none;
+            border-radius: 0px;
+            border-bottom: 2px solid #007bff;
+        }
+        }
+        @media(max-width:280px){
+            .login{
+                width: 100%;
+                padding: 0px !important;
+            }
+            .login .image img{
+              display: none;
+           }
+           .login form{
+               padding-left: 0px !important;
+               padding: 0px !important;
+               width: 100% !important;
+               margin-left:10%;
+               margin-top: 50% !important;
+           }
+           .login form h2{
+               position: absolute;
+               top: 20%;
+               left: 34%;
+               font-size: 18px;
+           }
+           #login input {
+            outline: none;
+            width: 600px;
+            width: 100% !important;
+            padding: 4px;
+            margin-left: 0px;
+            border: none;
+            border-radius: 0px;
+            border-bottom: 2px solid #007bff;
+        }
         }
    </style>
    
